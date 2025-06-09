35779f005bf3d5c7d4071fde2e8c4b47efe2a27e
Gladimir Jacobsen <gladimir.jacobsen_ex@scotiabank.cl>
2025-06-09 13:25:08 -0400

    add banner

--- solid-app/src/App.jsx
+++ solid-app/src/App.jsx
@@ -1,21 +1,14 @@
-import styles from './App.module.css';
+import banner from './assets/banner.png';
 
 function App() {
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <p>
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href='https://github.com/solidjs/solid'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn Solid
-        </a>
+    <div>
+      <header>
+        <h1>Shop shop</h1>
       </header>
+      <div>
+        <img src={banner} alt='site baner' />
+      </div>
     </div>
   );
 }
