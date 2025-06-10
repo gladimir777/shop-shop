d09b568446b4950f399b31a0bcd55eb232ea3d38
Gladimir Jacobsen <gladimir.jacobsen_ex@scotiabank.cl>
2025-06-09 20:07:31 -0400

    add tailwin css

--- solid-app/src/App.jsx
+++ solid-app/src/App.jsx
@@ -3,13 +3,18 @@ import Card from './components/Card';
 
 function App() {
   return (
-    <div>
+    <div class='container m-auto'>
       <header>
         <h1>Shop shop</h1>
       </header>
       <div>
-        <img src={banner} alt='site baner' />
-        <Card />
+        <img class='rounded-md' src={banner} alt='site baner' />
+        <div class='grid grid-cols-4 gap-10 my-4'>
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
       </div>
     </div>
   );
--- solid-app/src/components/Card.jsx
+++ solid-app/src/components/Card.jsx
@@ -1,11 +1,12 @@
 export default function Card() {
   return (
-    <div>
+    <div class='bg-white p-4 text-center rounded-md shadow-sm'>
       <h2>Card component</h2>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum aut,
         perferendis reiciendis et cupiditate numquam.
       </p>
+      <button class='btn'>Click me!</button>
     </div>
   );
 }
