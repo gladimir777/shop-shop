09cd3f600c9b1bab32c3948cd21177b08468c702
infogtechht <gladimirjacobsen@gmail.com>
2025-07-02 09:53:34 -0400

    delete static card component

--- solid-app/src/pages/Home.jsx
+++ solid-app/src/pages/Home.jsx
@@ -11,38 +11,6 @@ function Home() {
   return (
     <Show when={products()} fallback={<p>Loading...</p>}>
       <div class='grid grid-cols-4 gap-10 my-4'>
-        {/* <Card title='Shop Shop' rounded={true} flat={false}>
-          <h2>Shop shop</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum aut,
-            perferendis reiciendis et cupiditate numquam.
-          </p>
-          <button class='btn'>Click me!</button>
-        </Card>
-        <Card title='Shop Shop' rounded={false} flat={true}>
-          <h2>Shop shop</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum aut,
-            perferendis reiciendis et cupiditate numquam.
-          </p>
-          <button class='btn'>Click me!</button>
-        </Card>
-        <Card title='Shop Shop' rounded={true} flat={false}>
-          <h2>Shop Shop</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum aut,
-            perferendis reiciendis et cupiditate numquam.
-          </p>
-          <button class='btn'>Click me!</button>
-        </Card>
-        <Card title='Shop Shop' rounded={true} flat={false}>
-          <h2>Shop</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum aut,
-            perferendis reiciendis et cupiditate numquam.
-          </p>
-          <button class='btn'>Click me!</button>
-        </Card> */}
         <For each={products()}>
           {(product) => (
             <Card title='Shop Shop' rounded={true} flat={false}>
